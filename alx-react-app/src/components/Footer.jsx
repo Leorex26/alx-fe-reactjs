658931aa1b86aa60ed711e22dcc6591235a80352
Effiong Ephraim <Effiongephraim95@gmail.com>
2025-02-22 23:34:02 -0800

    Added UserProfile component with props

--- alx-react-app/src/components/Footer.jsx
+++ alx-react-app/src/components/Footer.jsx
@@ -1,6 +1,6 @@
 function Footer() {
     return (
-        <footer style={{ backgroundColor: '#222', color: 'white', textAlign: 'center', padding: '10px', marginTop: '20px' }}>
+        <footer>
             <p>© 2023 City Lovers</p>
         </footer>
     );
