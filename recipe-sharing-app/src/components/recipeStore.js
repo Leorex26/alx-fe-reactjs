ac9c9e627269174a3dff63ad91bf2c6602e0055d
Effiong Ephraim <152818965+Leorex26@users.noreply.github.com>
2025-03-02 08:35:13 -0800

    Delete recipe-sharing-app directory

--- recipe-sharing-app/src/components/recipeStore.js
+++
@@ -1,9 +0,0 @@
-import { create } from 'zustand';
-
-const useRecipeStore = create((set) => ({
-  recipes: [],
-  addRecipe: (recipe) => set((state) => ({ recipes: [...state.recipes, recipe] })),
-  removeRecipe: (id) => set((state) => ({ recipes: state.recipes.filter(r => r.id !== id) })),
-}));
-
-export default useRecipeStore;
