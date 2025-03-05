750697841c00da3a60420a6691d00745a35229dc
Effiong Ephraim <Effiongephraim95@gmail.com>
2025-03-05 08:38:27 -0800

    Fixed controlled components in RegistrationForm.jsx

--- form-handling-react/src/components/RegistrationForm.jsx
+++ form-handling-react/src/components/RegistrationForm.jsx
@@ -35,17 +35,32 @@ const RegistrationForm = () => {
       <h2>Register</h2>
       <div>
         <label>Username:</label>
-        <input type="text" name="username" value={formData.username} onChange={handleChange} />
+        <input 
+          type="text" 
+          name="username" 
+          value={formData.username} 
+          onChange={handleChange} 
+        />
         {errors.username && <p style={{ color: 'red' }}>{errors.username}</p>}
       </div>
       <div>
         <label>Email:</label>
-        <input type="email" name="email" value={formData.email} onChange={handleChange} />
+        <input 
+          type="email" 
+          name="email" 
+          value={formData.email} 
+          onChange={handleChange} 
+        />
         {errors.email && <p style={{ color: 'red' }}>{errors.email}</p>}
       </div>
       <div>
         <label>Password:</label>
-        <input type="password" name="password" value={formData.password} onChange={handleChange} />
+        <input 
+          type="password" 
+          name="password" 
+          value={formData.password} 
+          onChange={handleChange} 
+        />
         {errors.password && <p style={{ color: 'red' }}>{errors.password}</p>}
       </div>
       <button type="submit">Register</button>
