b23f46da321e941ddc65fc3e7b16862946d501c6
Effiong Ephraim <Effiongephraim95@gmail.com>
2025-03-08 08:41:06 -0800

    Add Updates

--- react-todo/src/_tests_/TodoList.test.js
+++ react-todo/src/_tests_/TodoList.test.js
@@ -1,7 +1,34 @@
-import { render, screen } from "@testing-library/react";
+import { render, screen, fireEvent } from "@testing-library/react";
 import TodoList from "../components/TodoList";
 
 test("renders TodoList component", () => {
   render(<TodoList />);
-  expect(screen.getByText(/todo list/i)).toBeInTheDocument();
+  expect(screen.getByText(/Todo List/i)).toBeInTheDocument();
+});
+
+test("adds a new todo", () => {
+  render(<TodoList />);
+  const input = screen.getByPlaceholderText("Add a new todo");
+  const button = screen.getByText("Add");
+
+  fireEvent.change(input, { target: { value: "New Todo" } });
+  fireEvent.click(button);
+
+  expect(screen.getByText("New Todo")).toBeInTheDocument();
+});
+
+test("toggles todo completion", () => {
+  render(<TodoList />);
+  const todoItem = screen.getByText("Learn React");
+
+  fireEvent.click(todoItem);
+  expect(todoItem).toHaveStyle("text-decoration: line-through");
+});
+
+test("deletes a todo", () => {
+  render(<TodoList />);
+  const deleteButton = screen.getAllByText("Delete")[0];
+
+  fireEvent.click(deleteButton);
+  expect(screen.queryByText("Learn React")).not.toBeInTheDocument();
 });
