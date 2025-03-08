e2705a09ffb5806563fcc7ff42292d3fbad1d430
Effiong Ephraim <Effiongephraim95@gmail.com>
2025-03-08 08:56:46 -0800

    Fix: Import TodoList in App.jsx and implement tests

--- react-todo/src/_tests_/TodoList.test.js
+++ react-todo/src/_tests_/TodoList.test.js
@@ -16,19 +16,3 @@ test("adds a new todo", () => {
 
   expect(screen.getByText("New Todo")).toBeInTheDocument();
 });
-
-test("toggles todo completion", () => {
-  render(<TodoList />);
-  const todoItem = screen.getByText("Learn React");
-
-  fireEvent.click(todoItem);
-  expect(todoItem).toHaveStyle("text-decoration: line-through");
-});
-
-test("deletes a todo", () => {
-  render(<TodoList />);
-  const deleteButton = screen.getAllByText("Delete")[0];
-
-  fireEvent.click(deleteButton);
-  expect(screen.queryByText("Learn React")).not.toBeInTheDocument();
-});
