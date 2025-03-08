21a25cbeff5f86aef728de1e8656370fc9675dd2
Effiong Ephraim <Effiongephraim95@gmail.com>
2025-03-07 23:25:52 -0800

    Add new changes

--- react-router-advanced/src/App.jsx
+++ react-router-advanced/src/App.jsx
@@ -1,3 +1,23 @@
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import Home from "./components/Home";
+import Profile from "./components/Profile";
 import UserProfile from "./components/UserProfile";
+import ProtectedRoute from "./components/ProtectedRoute";
 
-<Route path="/user/:id" element={<UserProfile />} />
+function App() {
+  return (
+    <Router>
+      <Routes>
+        <Route path="/" element={<Home />} />
+
+        {/* Protected Profile Route */}
+        <Route path="/profile" element={<ProtectedRoute><Profile /></ProtectedRoute>} />
+
+        {/* Dynamic Route for User Profiles */}
+        <Route path="/user/:id" element={<UserProfile />} />
+      </Routes>
+    </Router>
+  );
+}
+
+export default App;
