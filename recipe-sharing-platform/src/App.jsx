391906dad7527524a66c3b1f2e1dc4d823ea79a0
Effiong Ephraim <Effiongephraim95@gmail.com>
2025-03-16 16:35:56 -0700

    Add changes

--- recipe-sharing-platform/src/App.jsx
+++ recipe-sharing-platform/src/App.jsx
@@ -14,7 +14,7 @@ function App() {
       <p>Tailwind CSS is working correctly!</p>
     </div>
 
-    <div className="grid grid-cols-1 gap-6 md:grid-cols-2 lg:grid-cols-3">
+    <div className="bg-gray-100 min-h-screen">
       <HomePage />
     </div>
     
