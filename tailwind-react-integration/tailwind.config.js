9b9b902036478e1d32266dc86c5d4aac817e0c9d
Effiong Ephraim <Effiongephraim95@gmail.com>
2025-03-16 15:36:03 -0700

    Add updated changes

--- tailwind-react-integration/tailwind.config.js
+++ tailwind-react-integration/tailwind.config.js
@@ -1,5 +1,6 @@
+/** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: ["./src/**/*.{js,jsx,ts,tsx}", "./public/index.html"],
+  content: ["./src/**/*.{js,jsx,ts,tsx}", "./public/index.html"], // New way to specify purge paths
   theme: {
     extend: {},
   },
