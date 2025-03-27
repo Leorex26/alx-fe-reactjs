e15a53ad0c191e099aa3cd76557f3a4db400cb4b
Effiong Ephraim <Effiongephraim95@gmail.com>
2025-03-27 14:47:17 -0700

    Add components features

--- github-user-search/src/App.jsx
+++ github-user-search/src/App.jsx
@@ -2,6 +2,9 @@ import { useState } from 'react'
 import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import './App.css'
+import axios from "axios";
+import SearchBar from "./components/SearchBar";
+import UserProfile from "./components/UserProfile";
 
 function App() {
   const [count, setCount] = useState(0)
