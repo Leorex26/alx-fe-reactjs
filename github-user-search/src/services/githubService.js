101e2400801e23438af987565d3237b51a13e5e6
Effiong Ephraim <Effiongephraim95@gmail.com>
2025-03-28 14:49:39 -0700

    Advanced API Integration included

--- github-user-search/src/services/githubService.js
+++ github-user-search/src/services/githubService.js
@@ -1,5 +1,7 @@
 import axios from "axios";
 
+const BASE_URL = "https://api.github.com";
+
 export const fetchUserData = async ({ username, location, repos }) => {
   try {
     let query = `q=${username}`;
